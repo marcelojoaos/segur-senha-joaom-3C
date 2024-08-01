34e85d08e5aea260339754de0a051b65e8c34131
João Marcelo <108005320+marcelojoaos@users.noreply.github.com>
2024-08-01 16:35:50 -0300

    xxxxx

---
+++ main.js
@@ -0,0 +1,18 @@
+const numeroSenha = document.queryselector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+  tamanhoSenha = tamanhoSenha + 1;
+  numeroSenha.textContent = tamanhoSenha;  
+}
