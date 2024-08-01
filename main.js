e71b98225a7d8d820d89cf4f5f5d6f85fa289787
João Marcelo <108005320+marcelojoaos@users.noreply.github.com>
2024-08-01 17:33:20 -0300

    aaaa

--- main.js
+++ main.js
@@ -38,11 +38,25 @@ const simbolos = '!@#$%&?';
 geraSenha();
 
 function geraSenha(){
+    let alfabeto = '';
+    if(checkbox[0].checked){
+    alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if(checkbox[1].checked){
+    alfabeto = alfabeto + letrasMinusculas;
+    }
+    if(checkbox[2].checked){
+    alfabeto = alfabeto + numeros;
+    }
+    if(checkbox[3].checked){
+    alfabeto = alfabeto + simbolos;
+    }
+    
     let senha = '';
     for ( let i = 0; i < tamanhoSenha; i++){
-        let numeroAleatorio = math.randon()*letrasMaiusculas.length;
-     numeroAleatorio = math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
+    let numeroAleatorio = Math.random()*alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
     }
