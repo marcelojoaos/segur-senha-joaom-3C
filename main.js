fb27e2b7876d87842a757aa0920d8f4e9376e57d
João Marcelo <108005320+marcelojoaos@users.noreply.github.com>
2024-08-08 17:02:27 -0300

    xxxxx

--- main.js
+++ main.js
@@ -68,6 +68,8 @@ function classificaSenha() {
             forcaSenha.classList.add('forte');
             }else if (tamanhoSenha > 5 && tamanhoSenha < 12){
                 forcaSenha.classList.add('media');
+            }else if (tamanhoSenha<=5){
+                forcaSenha.classList.add('fraca');
             }
 
 }
